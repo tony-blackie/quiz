4f1b93b381d3e14129553a65ce3179b527a17c9e
Tony Spaskyi <jim.casey321@gmail.com>
2016-02-13 23:36:50 +0200

    Added questions

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,23 @@
 (function(){
+	"use strict";
 
-	// script goes here
+	angular
+		.module('myQuiz', [])
+		.controller('QuizController', QuizController);
 
+	QuizController.$inject = ['$scope', '$http', '$sce'];
+
+	function QuizController($scope, $http, $sce) {
+		var vm = this;
+
+		vm.score = 0;
+		vm.activeQuestion = -1;
+		vm.activeQuestionAnswered = 0;
+		vm.percentage = 0;
+
+		$http.get('quiz_data.json').then(function(quizData) {
+			vm.myQuestions = quizData.data;
+			vm.totalQuestions = vm.myQuestions.length;
+		});
+	}
 })();
